5f8dbc24112a518a08fad38b0a72c97d063bd1f7
Admin <ponomarenko02041993@gmail.com>
2019-08-25 17:21:33 +0300

    new

--- js/main.js
+++ js/main.js
@@ -44,4 +44,41 @@ $(document).ready(function () {
     $('.header-input-block').on('click',function () {
        $(this).closest('.item-filters').find('.list-filters-input').slideToggle('fast');
     });
+
+    /*-------------click link sort---------------*/
+
+    $('.sorting-link').on('click', function () {
+       $('.sorting-link').removeClass('active-sort');
+       $(this).addClass('active-sort');
+    });
+
+    $('.switcher-btn').on('click', function (e) {
+        e.preventDefault();
+        $('.switcher-btn').removeClass('active-btn-list');
+        $(this).addClass('active-btn-list');
+    });
+
+/*--------------back to top-------------*/
+
+    $('.up-btn').on('click', function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+
+    $(".square-button").on('click', function () {
+        $(this).closest('.wrapper-sort-catalog').find('.list-button').removeClass('active-btn-list');
+        $(this).closest('.wrapper-sort-catalog').find('.container-card').removeClass('sort-block-li');
+        $(this).closest('.wrapper-sort-catalog').find('.list-catalog-sort').removeClass('display-inline');
+        $(this).addClass('active-btn-list');
+
+    });
+
+    $(".list-button").on('click', function () {
+        $(this).closest('.wrapper-sort-catalog').find('.square-button').removeClass('active-btn-list');
+        $(this).addClass('active-btn-list');
+        $(this).closest('.wrapper-sort-catalog').find('.list-catalog-sort').addClass('display-inline');
+        $(this).closest('.wrapper-sort-catalog').find('.container-card').addClass('sort-block-li')
+    });
 });
